5b77cfe616a648e0c6e5c442461522ea40fc7bad
DCHRKV <dchurakov97@gmail.com>
2022-10-03 21:12:52 +0300

    Initial Commit

---
+++ Courses/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  Courses
+//
+//  Created by Dmytro Churakov on 02.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+            VStack(alignment: .leading, spacing: 8.0) {
+                Text("SwiftUI for iOS 15")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Text("20 sections - 3 hours".uppercased())
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.secondary)
+                Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundStyle(.secondary)
+            }
+        }
+        .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
+        .padding(.vertical, 20)
+        .frame(height: 350.0)
+        .background(Color("Background"))
+        .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .padding(.horizontal, 20)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            
+    }
+}
---
+++ Courses/CoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesApp.swift
+//  Courses
+//
+//  Created by Dmytro Churakov on 02.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
