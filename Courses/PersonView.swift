6703465ac82c6c1e478e5e1b53d5cfb3ed2bc6b0
DCHRKV <dchurakov97@gmail.com>
2022-10-04 23:55:15 +0300

    fixed last merge

--- Courses/PersonView.swift
+++ Courses/PersonView.swift
@@ -8,41 +8,75 @@
 import SwiftUI
 
 struct PersonView: View {
+    
+    
     var body: some View {
         NavigationView {
             List {
-                VStack(spacing: 10) {
-                    Image(systemName:
-                            "person.crop.circle.fill.badge.checkmark")
-                    .symbolVariant(.circle.fill)
-                    .font(.system(size: 30))
-                    .symbolRenderingMode(.palette)
-                    .foregroundStyle(.blue, .blue.opacity(0.3))
-                    .padding()
-                    .background(Circle().fill(.ultraThinMaterial))
-                    .background(
-                        Image("Background 7")
-                            .offset(x: -50, y: -330)
-                    )
-                    Text("Dmytro Churakov")
-                        .font(.title.weight(.bold))
-                    Text("dchurakov97gmail.com")
-                        .font(.footnote)
+                profile
+                menu
+                links
+                
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Account")
+        }
+    }
+    
+    
+    var profile: some View {
+        VStack(spacing: 10) {
+            Image(systemName:
+                    "person.crop.circle.fill.badge.checkmark")
+            .symbolVariant(.circle.fill)
+            .font(.system(size: 30))
+            .symbolRenderingMode(.palette)
+            .foregroundStyle(.blue, .blue.opacity(0.3))
+            .padding()
+            .background(Circle().fill(.ultraThinMaterial))
+            .background(
+                Image("Background 7")
+                    .offset(x: -50, y: -330)
+            )
+            Text("Dmytro Churakov")
+                .font(
+                    .title.weight(.bold)
+                )
+                .fontWeight(.thin)
+            Text("dchurakov97gmail.com")
+                .font(.footnote)
+                .foregroundStyle(.secondary)
+            Text("Ukraine")
+                .font(.title.weight(.semibold))
+                .foregroundStyle(.linearGradient(colors: [.blue, .yellow], startPoint: .topLeading, endPoint: .bottomTrailing))
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+    }
+    
+    
+    var menu: some View {
+        Section {
+            NavigationLink(destination: ContentView()) {
+                Label("Courses", systemImage: "book")
+            }
+            Label("Settings", systemImage: "gearshape")
+            Label("Payment", systemImage: "creditcard.and.123")
+            Label("Help", systemImage: "questionmark.circle")
+        }
+        .listRowSeparator(.hidden)
+    }
+    
+    
+    var links: some View {
+        Section {
+            Link(destination: URL(string: "https://designcode.io")!) {
+                HStack {
+                    Label("Open website", systemImage: "safari")
+                    Spacer()
+                    Image(systemName: "link")
                         .foregroundStyle(.secondary)
-                    Text("Ukraine")
-                        .font(.title.weight(.semibold))
-                        .foregroundStyle(.linearGradient(colors: [.blue, .yellow], startPoint: .topLeading, endPoint: .bottomTrailing))
                 }
-                .frame(maxWidth: .infinity)
-                .padding()
-                Section {
-                    Label("Settings", systemImage: "gearshape")
-                    Label("Payment", systemImage: "creditcard.and.123")
-                    Label("Help", systemImage: "questionmark.circle")
-                }
-                .listStyle(.insetGrouped)
-                .listRowSeparator(.hidden)
-                .navigationTitle("Account")
             }
         }
     }
