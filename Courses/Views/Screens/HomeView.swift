b26aa68ff4b5d0adc4d91b7a320c4e7ca3a237eb
DCHRKV <dchurakov97@gmail.com>
2022-10-12 21:24:23 +0300

    Tabview pagination added

--- Courses/Views/Screens/HomeView.swift
+++ Courses/Views/Screens/HomeView.swift
@@ -11,33 +11,52 @@ struct HomeView: View {
     @State var hasScrolled = false
     
     var body: some View {
-        ScrollView {
-            GeometryReader { proxy in
-                Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
+        ZStack {
+            Color("Background").ignoresSafeArea()
+            
+            ScrollView {
+                scrollDetection
+                featured
+                Color.clear.frame(height: 1000)
             }
-            .frame(height: 0)
-            
-            FeaturedItem()
-            
-            Color.clear.frame(height: 1000)
-        }
-        .coordinateSpace(name: "scroll")
-        .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
-            withAnimation(.easeInOut) {
-                if value < 0 {
-                    hasScrolled = true
-                } else {
-                    hasScrolled = false
+            .coordinateSpace(name: "scroll")
+            .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
+                withAnimation(.easeInOut) {
+                    if value < 0 {
+                        hasScrolled = true
+                    } else {
+                        hasScrolled = false
+                    }
                 }
-            }
-            
-        })
-        .safeAreaInset(edge: .top, content: {
-            Color.clear.frame(height: 70)
-        })
-        .overlay(
-            NavigationBar(title: "Featured", hasScrolled: $hasScrolled)
+                
+            })
+            .safeAreaInset(edge: .top, content: {
+                Color.clear.frame(height: 70)
+            })
+            .overlay(
+                NavigationBar(title: "Featured", hasScrolled: $hasScrolled)
         )
+        }
+    }
+    
+    private var scrollDetection: some View {
+        GeometryReader { proxy in
+            Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
+        }
+        .frame(height: 0)
+    }
+    
+    private var featured: some View {
+        TabView {
+            ForEach(courses) { item in
+                FeaturedItem(course: item)
+            }
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .frame(height: 430)
+        .background(
+            Image("Blob 1")
+                .offset(x: 20))
     }
 }
 
